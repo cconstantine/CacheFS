de037b1aec50532478c02a7b27c29636f74ee849
Chris Constantine <cconstan@gmail.com>
2011-03-15 23:29:22 -0700

    tweeks

--- cachefs.py
+++ cachefs.py
@@ -112,7 +112,7 @@ class FileDataCache:
 
         self.path_id = None
 
-        self.db.execute('INSERT OR IGNORE INTO paths values (NULL,?)', (self.path,))
+        self.db.execute('INSERT OR IGNORE INTO paths (path) values (?)', (self.path,))
 
         for pid, in self.db.execute('select id from paths where path = ?', (self.path,)):
             self.path_id = pid
@@ -434,7 +434,6 @@ class CacheFS(fuse.Fuse):
 def create_db(cache_dir):
     cache_db = sqlite3.connect(os.path.join(cache_dir, "metadata.db"), isolation_level="DEFERRED")
     cache_db.execute('create table if not exists paths (id  INTEGER PRIMARY KEY, path STRING, UNIQUE(path))')
-    cache_db.execute('create index if not exists path_index on paths (path)') 
 
 
     cache_db.execute('create table if not exists file_data ( path_id INTEGER  not null, offset integer, end integer, FOREIGN KEY(path_id) REFERENCES paths(id) )')
